68bf45d1dc033ad93646b9d9c05884b4cdd1f636
Marco Abundo <shrtlist@gmail.com>
2025-01-29 21:15:33 -0800

    Exclude country

--- ContactSearch/Extensions/CNPostalAddress+Extension.swift
+++ ContactSearch/Extensions/CNPostalAddress+Extension.swift
@@ -1,6 +1,6 @@
 //
-//  AddressController.swift
-//  Sidewalk
+//  CNPostalAddressz+Extension.swift
+//  ContactSearch
 //
 //  Created by Marco Abundo on 3/2/19.
 //  Copyright 2019 shrtlist.com. All rights reserved.
@@ -10,6 +10,16 @@ import Contacts
 
 extension CNPostalAddress {
     var addressString: String {
-        return CNPostalAddressFormatter.string(from: self, style: .mailingAddress)
+        let mutableAddress = CNMutablePostalAddress()
+        mutableAddress.street = self.street
+        mutableAddress.city = self.city
+        mutableAddress.state = self.state
+        mutableAddress.postalCode = self.postalCode
+        mutableAddress.subLocality = self.subLocality
+        mutableAddress.subAdministrativeArea = self.subAdministrativeArea
+        // Exclude country (don't set `mutableAddress.country`)
+
+        let formatter = CNPostalAddressFormatter()
+        return formatter.string(from: mutableAddress).trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
